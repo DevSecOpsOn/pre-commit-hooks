78cabd112e4c31da98262cd5aeba16724d10239d
Rodrigo Carvalho <rdgacarvalho@gmail.com>
2024-04-07 16:28:21 +0200

    CU-86by7z131 - Split scritps per functins
    * common.sh script functions
    
    * add gitignore file
    
    * add editorconfig
    
    Signed-off-by: Rodrigo Carvalho <rdgacarvalho@gmail.com>

--- hooks/pluto_detect_files.sh
+++ hooks/pluto_detect_files.sh
@@ -7,54 +7,14 @@ declare -a ARGS=()
 declare -a FILES=()
 declare -a PARAMS="detect-files --output markdown"
 
+readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+. "$SCRIPT_DIR/common.sh"
+
 main() {
 
   initialize_
   parse_cmdline_ "$@"
   pluto_detect_files_ "$ARGS" "$FILES"
-
-}
-
-initialize_() {
-
-  local dir
-  local source
-  source="${BASH_SOURCE[0]}"
-
-  while [[ -L $source ]]; do
-    dir="$(cd -P "$(dirname "$source")" > /dev/null && pwd)"
-    source="$(readlink "$source")"
-    [[ $source != /* ]] && source="$dir/$source"
-  done
-  _SCRIPT_DIR="$(dirname "$source")"
-
-  # https://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html#index-getopts
-  # source getopt function and shellcheck source=lib_getopt
-  . "$_SCRIPT_DIR/lib_getopt"
-
-}
-
-parse_cmdline_() {
-  declare argv
-  argv=$(getopt -n Pluto -o hi: --long help,directory: -- "$@") || return
-  eval "set -- $argv"
-
-  for argv; do
-    case $1 in
-      -d | --directory)
-        ARGS+=("$1")
-        ARGS+=("$2")
-        shift 2
-        ;;
-      --)
-        shift
-        FILES+=("$@")
-        break
-        ;;
-     *)
-        shift
-    esac
-  done
 
 }
 
@@ -83,4 +43,3 @@ pluto_detect_files_() {
 }
 
 [[ ${BASH_SOURCE[0]} != "$0" ]] || main "$@"
-
